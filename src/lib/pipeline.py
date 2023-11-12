537d2877d5f78a0ccdf7477b50193dde20e3b28f
Benjamin Vera <bevera@student.ethz.ch>
2023-11-12 14:28:50 +0100

    Finalize project 1

--- src/lib/pipeline.py
+++ src/lib/pipeline.py
@@ -1,27 +1,50 @@
-from sklearn import preprocessing
-import pandas as pd
-from sklearn.impute import SimpleImputer, KNNImputer
-from pyod.models.inne import INNE
 import numpy as np
-import umap
+import pandas as pd
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_X_y
+from sklearn.neighbors import KernelDensity
 
 
-def get_preprocessors(df):
-    scaler = preprocessing.RobustScaler().fit(df)
-    imputer = KNNImputer(missing_values=np.nan, weights="distance").fit(df)
-    return scaler, imputer
+class oRegressor(BaseEstimator, RegressorMixin):
+    def __init__(self, model, thresh):
+        self.model = model
+        self.thresh = thresh
 
+    def fit(self, X, y):
+        # Check that X and y have correct shape
+        # X, y = check_X_y(X, y)
+        X_rm, y_rm = self._rm_outliers(X, y)
+        self.model.fit(X_rm, y_rm)
 
-def reduce_dimension(df):
-    reducer = umap.UMAP(n_neighbors=10, n_components=8)
-    embedding = reducer.fit_transform(df)
-    return pd.DataFrame(embedding)
+        return self
 
+    def _rm_outliers(self, X, y):
+        print("Hello")
 
-def detect_outliers(df):
-    clf = INNE(n_estimators=300)
-    df_np = np.array(df)
-    clf.fit(df_np)
-    print(pd.Series(clf.labels_).value_counts())
+        df = pd.DataFrame(X)
+        num_obs_before = df.shape[0]
+        X_cols = df.columns
+        self.model.fit(X, y)
+        # df['y'] = y
+        df['predictions'] = self.model.predict(X)
+        df['residuals'] = df.apply(lambda x: abs(x['predictions'] - x['y']), axis=1)
+        df['prob'] = self._get_prob(y)
+        df['anomaly_score'] = df['prob'] * df['residuals']
 
-    return clf.labels_ == 0
+        print(df['anomaly_score'].describe())
+        subdf = df[df['anomaly_score'] < self.thresh]
+        # y_formatted = np.array(subdf['y']).ravel()
+        y_formatted = subdf['y']
+        print(f"#Outliers removed: {num_obs_before - subdf.shape[0]}")
+
+        return subdf[X_cols], y_formatted
+
+    def _get_prob(self, y):
+        array_y = np.array(y).reshape(-1, 1)
+        kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(array_y)
+        log_density = kde.score_samples(array_y)
+
+        return np.exp(log_density)
+
+    def predict(self, X):
+        return self.model.predict(X)
--- task1_final.py
+++ task1_final.py
@@ -1,78 +1,51 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  4 14:50:13 2023
-
-@author: yvesa
-"""
-
-import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
 import numpy as np
 import pandas as pd
-from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.impute import KNNImputer
 from xgboost import XGBRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.feature_selection import SelectFromModel
-from sklearn.model_selection import GridSearchCV
 
 # Read data from csv files
-df_x_train = pd.read_csv('./X_train.csv').drop(columns="id")
-df_y_train = pd.read_csv('./y_train.csv').drop(columns="id")
+df_x_train = pd.read_csv('./data/X_train.csv').drop(columns="id")
+df_y_train = pd.read_csv('./data/y_train.csv').drop(columns="id")
 
-# Split the data into training and validation sets
-X_train, X_val, y_train, y_val = train_test_split(df_x_train, df_y_train, test_size=0.2, random_state=42)
-
-# Define the model for feature selection
-model = XGBRegressor(n_estimators = 1000,
-                     learning_rate = 0.02,
-                     random_state = 42)
-
-
+# Define ML pipeline as follows
 # RobustScaler for outlier removing, default to Interquartile Range IQR)
 # KNNImputer to insert missing data according to k-nearest neighbors
-# SelectFromModel for feature selection (XGBRegressor)
+# SelectFromModel for feature selection: XGBRegressor
 # Regression model: XGBRegressor
 
-pipeline = Pipeline([
-    ('scalar', preprocessing.RobustScaler()),
-    ('imputer', KNNImputer(n_neighbors = 5)),
-    ('feature_selector', SelectFromModel(model)),
-    ('regression_model', XGBRegressor())])
+# Define model for feature selection
+feature_selector = XGBRegressor(
+    n_estimators = 1000,
+    learning_rate = 0.02,
+    random_state = 42
+)
 
-#%% Train the model
+pipe = Pipeline([
+('scaler', preprocessing.RobustScaler())
+, ('imputer', KNNImputer())
+, ('feature_selector', SelectFromModel(feature_selector))
+, ('regression_model', XGBRegressor())
+])
 
-# Parameters can be adapted. Including reg_lambda or reg_alpha didn't lead to any improvements against overfitting.
-# However, they were not deeply investigated
-parameters = {
-    'regression_model__n_estimators': [1000],
-    'regression_model__learning_rate': [0.005],
-    'regression_model__subsample': [0.6],
-}
+# Set parameters of the pipeline
+pipe.set_params(
+    imputer__missing_values=np.nan,
+    imputer__n_neighbors=5,
+    regression_model__device="gpu", # enables GPU acceleration for XGBoostRegressor
+    regression_model__n_estimators=1000,
+    regression_model__learning_rate=0.005,
+    regression_model__subsample=0.6,
+)
 
-gridmodel = GridSearchCV(pipeline, parameters, scoring='r2',
-                         n_jobs = -1, cv=3, return_train_score=True)
+# Train pipeline on entire training data
+pipe.fit(df_x_train, np.array(df_y_train).ravel())
 
-gridmodel.fit(X_train, y_train)
-
-y_train_pred = gridmodel.predict(X_train)
-y_val_pred = gridmodel.predict(X_val)
-
-val_score = round(r2_score(y_val, y_val_pred), 3)
-train_score = round(r2_score(y_train, y_train_pred), 3)
-print("Training score:", train_score)
-print("Validation score", val_score)
-
-#%% Prediction
-
+# Make predictions for submission
 df_sample = pd.read_csv('./sample.csv')
-
 df_sample.head()
-
 df_x_test = pd.read_csv('./X_test.csv').drop(columns="id")
-df_sample['y'] =gridmodel.predict(df_x_test)
-
-
+df_sample['y'] =pipe.predict(df_x_test)
 df_sample.to_csv("predictions.csv", index=False)
